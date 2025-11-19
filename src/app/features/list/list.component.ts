b24ede948432f7917d76c8012e3d76330340991b
marlos11 <marlos-gama@hotmail.com>
2025-11-18 21:35:08 -0300

    feat:adiciona listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  products: any[] = []
+  httpClient = inject(HttpClient)
 
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products/').subscribe((products) => {
+      this.products = products
+      console.log(products)
+    })
+  }
 }
