977b829ebf9ed3255fb1264bde036c3086e854a5
marlos11 <marlos-gama@hotmail.com>
2025-11-25 23:44:47 -0300

    feat:adiciona link para criar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,12 +4,14 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/Products.interface';
 import { CardComponent } from "./components/card/card.component";
+import { RouterLink } from "@angular/router";
+import { MatAnchor } from "@angular/material/button";
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatAnchor],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
