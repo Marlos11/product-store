2f62df2288a7c799ffde9d7b32304d4e9bd99508
marlos11 <marlos-gama@hotmail.com>
2025-12-02 09:30:11 -0300

    feature: cria  formulario para adicionar produtos

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
 
+  form = new FormGroup({
+    title: new FormControl<string>('', Validators.required)
+  })
+
+  onSubmit() {
+    this.form.controls.title.value
+  }
 }
